8567f266537076f958d570af4bf447ef5fd1c8ed
Denisse Guerra <denisse02dy@gmail.com>
2023-04-20 18:04:11 -0700

    Add files via upload

---
+++ Lab1/lab1.py
@@ -0,0 +1,17 @@
+#question 5
+
+"""
+    >>> When_will_I_graduate(2021)
+    2025
+    >>> When_will_I_graduate(2018)
+    2022
+    >>> When_will_I_graduate(2025)
+    2029
+    """	
+
+#y= year of start at college
+
+When_will_I_graduate = lambda y: sum((y,4))
+    
+
+        
